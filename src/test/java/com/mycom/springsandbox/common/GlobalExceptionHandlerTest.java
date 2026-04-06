8772c4abafad94ad016a7434b73d15a8bb97267e
codwithyc <su253294@gmail.com>
2026-04-06 17:19:22 +0900

    :white_check_mark: 공통 Response 규칙 변경사항 반영에 맞춰 테스트 수정

--- src/test/java/com/mycom/springsandbox/common/GlobalExceptionHandlerTest.java
+++ src/test/java/com/mycom/springsandbox/common/GlobalExceptionHandlerTest.java
@@ -19,20 +19,10 @@ class GlobalExceptionHandlerTest {
     @DisplayName("비지니스 예외 헨들링 테스트")
     void handleBusinessException() {
         // Given
-        GlobalExceptionHandler handler = new GlobalExceptionHandler();
-        BusinessException businessException = new BusinessException(ErrorCode.USER_NOT_FOUND, "잘못된 요청입니다.");
-        WebRequest req = mock(WebRequest.class);
-        when(req.getDescription(false)).thenReturn("uri=/api/user/1");
 
         // When
-        ResponseEntity<ApiError> response = handler.handleBusiness(businessException, req);
 
         // Then
-        // 예외가 올바르게 처리되었는지 검증합니다.
-        assertThat(response.getStatusCode().value()).isEqualTo(ErrorCode.USER_NOT_FOUND.getHttpStatus().value());
-        assertThat(response.getBody().code()).isEqualTo(ErrorCode.USER_NOT_FOUND.name());
-        assertThat(response.getBody().message()).isEqualTo(ErrorCode.USER_NOT_FOUND.getDefaultMessage());
-        assertThat(response.getBody().path()).isEqualTo("/api/user/1");
 
     }
 
